568474cb539880ae191a62fce2e81f247054a911
btylqr <143708099+btylqr@users.noreply.github.com>
2023-12-16 13:55:16 +0000

    .

--- zo/kch.fish
+++ zo/kch.fish
@@ -1,10 +1,10 @@
 #!/bin/fish
-/workspaces/lq-PostGresSqlPanty/lq/pnp.fish
-/workspaces/lq-PostGresSqlPanty/lq/fixrs.fish
-/workspaces/lq-PostGresSqlPanty/lq/st.sh
-/workspaces/lq-PostGresSqlPanty/lq/aryt.fish
-/workspaces/lq-PostGresSqlPanty/lq/tgpt.fish
-/workspaces/lq-PostGresSqlPanty/lq/bun.fish
-/workspaces/lq-PostGresSqlPanty/lq/tmuxx.fish
+/workspaces/zomeo/zo/pnp.fish
+/workspaces/zomeo/zo/fixrs.fish
+/workspaces/zomeo/zo/st.sh
+/workspaces/zomeo/zo/aryt.fish
+/workspaces/zomeo/zo/tgpt.fish
+/workspaces/zomeo/zo/bun.fish
+/workspaces/zomeo/zo/tmuxx.fish
 
 
